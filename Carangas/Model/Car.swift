1b02c9ec15b4df8e22911bdd90293bd764950989
Gustavo De Sousa <gustavodesousa@Gustavos-MacBook-Pro.local>
2018-12-26 11:38:08 -0200

    Rest Get

---
+++ Carangas/Model/Car.swift
@@ -0,0 +1,29 @@
+//
+//  Car.swift
+//  Carangas
+//
+//  Created by Gustavo De Sousa on 24/12/18.
+//  Copyright © 2018 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+class Car: Codable { 
+    var _id: String
+    var brand: String
+    var gasType: Int
+    var name: String
+    var price: Double
+    
+    var gas: String {
+        switch gasType {
+        case 0:
+            return "Flex"
+        case 1:
+            return "Alcool"
+        default:
+            return "Gasolina"
+        }
+    }
+    
+}
---
+++ Carangas/REST.swift
@@ -0,0 +1,74 @@
+//
+//  REST.swift
+//  Carangas
+//
+//  Created by Gustavo De Sousa on 24/12/18.
+//  Copyright © 2018 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+enum CarError {
+    case url
+    case taskError(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case invalidJSON
+}
+
+class REST {
+    
+    private static let basePath = "https://carangas.herokuapp.com/cars"
+    
+    private static let configuration: URLSessionConfiguration = {
+       let config = URLSessionConfiguration.default
+        //impede que o usuario baixa arquivos com o celular
+        config.allowsCellularAccess = false
+        //informar o tipo de dados
+        config.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        //30 seg para receber a sua requisicao
+        config.timeoutIntervalForRequest = 30.0
+        //limita a quantidade de conexao
+        config.httpMaximumConnectionsPerHost = 5
+        return config
+    }()
+    
+    private static let session = URLSession(configuration: configuration)
+    
+    class func loadCars(onComplete: @escaping ([Car]) -> Void, onError: @escaping (CarError) -> Void) {
+        guard let url = URL(string: basePath) else {
+            onError(.url)
+            return
+        }
+        
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse else {
+                    onError(.noResponse)
+                    return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data else {return}
+                    do{
+                        let cars = try JSONDecoder().decode([Car].self, from: data)
+                        onComplete(cars)
+                    } catch {
+                        print(error.localizedDescription)
+                        onError(.invalidJSON)
+                    }
+                    
+                    
+                } else {
+                    print("Algum status invalido pelo servidor")
+                    onError(.responseStatusCode(code: response.statusCode))
+                }
+                
+            } else {
+                print(error!)
+                onError(.taskError(error: error!))
+            }
+        }
+        dataTask.resume()
+    }
+}
